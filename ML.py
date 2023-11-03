553e9280e3b66c3dd78127930210605c4bf037f2
infocenter-repository <149783219+infocenter-repository@users.noreply.github.com>
2023-11-03 18:24:27 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
-import xgboost as xgb
-from sklearn.metrics import mean_squared_error
+# import xgboost as xgb
+# from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv('ML TEST.csv')
 df
