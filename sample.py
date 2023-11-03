e8cc10b5e1572ba9b315e20c73789d7204bba7ab
infocenter-repository <149783219+infocenter-repository@users.noreply.github.com>
2023-11-03 17:44:44 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title = "My Webpage", page_icon = ":tada:", layout = "wide")
+
+st.subheader("Hi, I am Aditi :wave:")
+st.title("A Data Analyst from India")
+st.write("I am passionate about finding ways to use Python and Machine Learning in a more efficient and effective ways")
