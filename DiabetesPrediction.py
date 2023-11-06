7083ab56dd8309825b73047156c1a8f3c424f948
infocenter-repository <149783219+infocenter-repository@users.noreply.github.com>
2023-11-06 15:07:52 +0530

    Create DiabetesPrediction.py

---
+++ DiabetesPrediction.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+# loading the saved model
+loaded_model = pickle.load(open('C:/Users/AditiKurde/Desktop/Streamlit/trained_model.sav', 'rb'))
+
+
+
+def diabetes_prediction(input_data):
+##    input_data = (5,166,72,19,175,25.8,0.587,51)
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+
+
+def main():
+    #Giving Title
+    st.title("Diabetes Prediction Web App")
+
+    #Getting input from users
+    Pregnancies = st.text_input("Number of Preganancies")
+    Glucose = st.text_input("Glucose Level")
+    BloodPressure = st.text_input("Blood Pressure Value")
+    SkinThickness = st.text_input("SkinThickness Value")
+    Insulin = st.text_input("Insulin Level")
+    BMI = st.text_input("BMI Value")
+    DiabetesPedigreeFunction = st.text_input(" Diabetes Pedigree Value")
+    Age = st.text_input("Age of the Person")
+
+    diagnosis = ''
+
+    if st.button('Diabetes Test Results'):
+        diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+
+
+    st.success(diagnosis)
+
+
+if __name__ == '__main__':
+    main()
